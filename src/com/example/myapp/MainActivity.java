fadbf54647a7ac2008d2ccee84abe0e2f23ab2ab
Uliana <like-me1@yandex.ru>
2012-11-04 20:49:38 +0100

    User interface updates

--- src/com/example/myapp/MainActivity.java
+++ src/com/example/myapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Adapter;
 
 public class MainActivity extends Activity {
 
@@ -17,4 +18,33 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+ /* public class SpinnerDemo extends Activity
+    implements Adapter.OnItemSelectedListener {
+    private TextView selection;
+    private static final String[] items={"Dresden", "Leipzig", "Hamburg", "Berlin"};
+@Override
+public void onCreate(Bundle icicle) {
+super.onCreate(icicle);
+setContentView(R.layout.main);
+selection=(TextView)findViewById(R.id.selection);
+Spinner spin=(Spinner)findViewById(R.id.spinner);
+spin.setOnItemSelectedListener(this);
+ArrayAdapter<String> aa=new ArrayAdapter<String>(this,
+android.R.layout.simple_spinner_item,
+items);
+aa.setDropDownViewResource(
+android.R.layout.simple_spinner_dropdown_item);
+spin.setAdapter(aa);
+}
+@Override
+public void onItemSelected(AdapterView<?> parent,
+View v, int position, long selection.setText(items[position]);
+id) {
+}
+@Override
+public void onNothingSelected(AdapterView<?> parent) {
+selection.setText("");
+}
+}
+*/
 }
