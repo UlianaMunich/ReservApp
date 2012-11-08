69f8c63205256e0eaccd4b1f24dd9f2fa51c8cd9
Uliana <like-me1@yandex.ru>
2012-11-08 22:18:38 +0100

    revertion

---
+++ src/com/example/myapp/ActivityNiarestRestauranes.java
@@ -0,0 +1,14 @@
+package com.example.myapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityNiarestRestauranes extends Activity {
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+
+}
