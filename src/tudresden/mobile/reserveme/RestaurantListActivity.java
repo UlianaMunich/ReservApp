fe6f210dba731afc8d140f2828d805ea320ae32f
dimakuv <dmitry.kuvayskiy@gmail.com>
2012-12-02 18:02:52 +0100

    now can connect to remote server

--- src/tudresden/mobile/reserveme/RestaurantListActivity.java
+++ src/tudresden/mobile/reserveme/RestaurantListActivity.java
@@ -3,16 +3,53 @@ package tudresden.mobile.reserveme;
 import java.util.ArrayList;
 import java.util.List;
 
+import tudresden.mobile.reserveme.MainActivity.GetCitiesTask;
 import tudresden.mobile.reserveme.backend.Restaurant;
 import tudresden.mobile.reserveme.backend.RestaurantManager;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class RestaurantListActivity extends Activity {
 
-	List<Restaurant> countryList= new ArrayList<Restaurant>();
+	List<Restaurant> restaurantsList = new ArrayList<Restaurant>();
 	String city;
+	
+	ListView lvRestaurants;
+	
+	
+	/* dimakuv: 
+	 *   Background Task for fetching list of restaurants from our backend server into Spinner
+	 */
+	public class GetRestaurantsTask extends AsyncTask<String, Void, RestaurantArrayAdapter> {
+		
+		@Override
+		protected RestaurantArrayAdapter doInBackground(String... cities) {
+			RestaurantArrayAdapter aa = null;
+
+			restaurantsList = RestaurantManager.getRestaurants(cities[0]);		
+
+			if (restaurantsList != null) {
+				aa = new RestaurantArrayAdapter(
+						getApplicationContext(), R.layout.listitem_restaurants, restaurantsList);
+			}	        
+	        
+			return aa;
+		}
+		
+		@Override
+		protected void onPostExecute(RestaurantArrayAdapter aa) {			
+			// NOTE: this method is executed in main, UI thread
+			if (aa != null) {
+				lvRestaurants.setAdapter(aa);			
+			} else {
+        	   	Toast.makeText(RestaurantListActivity.this, "WARNING: Didn't retrieve restaurants!", Toast.LENGTH_SHORT).show();		        	   
+           }
+		}
+	}
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -25,18 +62,19 @@ public class RestaurantListActivity extends Activity {
 		// Set the View layer
 		setContentView(R.layout.list_restaurants);
 		setTitle(this.city);
-
-		List<Restaurant> countryList = RestaurantManager.getRestaurants(this.city);		
-		
-		// Create a customized ArrayAdapter
-		RestaurantArrayAdapter adapter = new RestaurantArrayAdapter(
-				getApplicationContext(), R.layout.listitem_restaurants, countryList);
 		
 		// Get reference to ListView holder
-		ListView lv = (ListView) this.findViewById(R.id.listview_restaurants);
+		lvRestaurants = (ListView) this.findViewById(R.id.listview_restaurants);
 		
+        new GetRestaurantsTask().execute(city);
+
+		// Create a customized ArrayAdapter
+		// empty in the beginning
+		RestaurantArrayAdapter adapter = new RestaurantArrayAdapter(
+				getApplicationContext(), R.layout.listitem_restaurants, restaurantsList);
+				
 		// Set the ListView adapter
-		lv.setAdapter(adapter);
+		lvRestaurants.setAdapter(adapter);
 	}
 
 }
