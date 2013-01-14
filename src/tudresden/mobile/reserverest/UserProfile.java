a1ec0c5b101d3d12523e36ce16719d09c70df94f
Uliana <like-me1@yandex.ru>
2013-01-14 12:44:18 +0100

    user interface changed 14.01.2013

--- src/tudresden/mobile/reserverest/UserProfile.java
+++ src/tudresden/mobile/reserverest/UserProfile.java
@@ -2,14 +2,29 @@ package tudresden.mobile.reserverest;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class UserProfile extends Activity {
-
+	private Button payDishes;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_user_profile);
+		
+		//button to Activity with different methods of payment
+		payDishes = (Button)findViewById(R.id.pay_dishes);
+	    payDishes.setOnClickListener(new View.OnClickListener() {						
+	    	   	@Override
+				public void onClick(View v) {
+	    	   		// open Google Maps view
+					Intent intent = new Intent(UserProfile.this, Payment.class);
+		        	startActivity(intent);
+				}
+			});
 	}
 
 	@Override
